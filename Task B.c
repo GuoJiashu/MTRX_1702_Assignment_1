3fe40ccb32da2ed8b2d6588cc899cc6117d73732
郭嘉树 <132477874+GuoJiashu@users.noreply.github.com>
2023-08-29 04:39:41 +1000

    Task B.c

--- Task B.c
+++ Task B.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 // Deal with strings through this library
 #include <string.h>
+// Deal with strings through this library
 #include <stdlib.h>
 
 // Accept arguments from the terminal
@@ -9,19 +10,21 @@ int main(int argc, char *argv[]) {
     // Here, to judge the number of arguments, if the number is not equal to two, it will print a message
     if (argc != 5) {
         printf("Incorrect number of arguments!\n");
-        // When the code finds not correct inputs here, it will quit and return 1 which means not normal "shut down" or "error" occurs here
+        // When the code finds not correct inputs here, it will quit and return 1 which means not normal "shut down" or
+        // "error" occurs here
         return 1;
     }
 
     // Here creat a string, and it stores 26 characters
     char initial_board[26];
     // Use function strncpy to copy the information from the second arguments
-    // Only get 25 characters from the arguments because our game is 5x5, it will make sure we just deal with 25 characters
+    // Only get 25 characters from the arguments because our game is 5x5, will make sure we just deal with 25 characters
     strncpy(initial_board, argv[1], 25);
-    // It is an important one, it set an ending character(26th) '\0', the purpose is to make that the data stored in initial_board is a valid string, because the string in C must end with NULL
+    // It is an important one, it set an ending character(26th) '\0', the purpose is to make that the data stored in
+    // initial_board is a valid string, because the string in C must end with NULL
     initial_board[25] = '\0';
 
-    // Atoi function convert string from arguments to integer
+    // Atoi function converts arguments from strings to integers
     int player = atoi(argv[2]);
     int row = atoi(argv[3]);
     int col = atoi(argv[4]);
@@ -37,6 +40,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    // This will detect the exact position about what has user input.
     int index = row * 5 + col;
 
     // Check if the cell is already occupied
@@ -45,11 +49,12 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    // Determine which symbol ('X', 'O') will be put there
     char playerToken = (player == 1) ? 'X' : 'O';
+    // This will put the one of the symbol to the position that asked
     initial_board[index] = playerToken;
 
     // This is a loop, it will over all the character inside the initial_board, and to check are they valid characters
-    // If I find any character not 'X' or 'O' or '*', it will print a message and then return 1(same function like above)
     for (int i = 0; i < 25; i++) {
         char c = initial_board[i];
         if (c != 'X' && c != 'O' && c != '*' && c != 'E') {
@@ -57,13 +62,15 @@ int main(int argc, char *argv[]) {
             return 1;
         }
     }
-
+    
+    // This is a 2-D array, to represent eight different directions around the given position
     int directions[8][2] = {
             {-1, 0}, {-1, -1}, {-1, 1},
-            {0, -1},                       {0, 1},
+            {0, -1},                        {0, 1},
             {1, 0},  {1, -1}, {1, 1}
     };
 
+    // Update the surrounding cells
     for (int d = 0; d < 8; d++) {
         int newRow = row + directions[d][0];
         int newCol = col + directions[d][1];
@@ -77,7 +84,6 @@ int main(int argc, char *argv[]) {
     }
 
     // Produces game board
-    // This is the declaration for the variable will used in the next
     int i, j;
 
     // The outer frame
