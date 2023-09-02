606206e4d51fe3433d4b13ee9ec45d4de9bd955c
郭嘉树 <132477874+GuoJiashu@users.noreply.github.com>
2023-09-02 23:21:09 +1000

    Update Task C.c

--- Task C.c
+++ Task C.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
+/*!
+  This program implements a simple two-player game called "Bricker" played on a 5x5 grid. This program let two players to
+  play take turns on a 5x5 game board. Different player is represented by 'X' and 'O'. The player needs to start game on
+  terminal (command line). The game starts with an empty or a customized board, the customized information is from command
+  lines. When program starts, it asks for the input from players, players enter the specified data of row and column. The
+  program checks the input to ensure valid moves and checks for game termination conditions. If a player's move is valid,
+  their symbol is placed on the give position on the board, and the surrounding cells are updated accordingly. The game
+  continues until there are no more empty cells. The program will notice players whether the inputs are valid or not. Upon game
+  completion, it identifies the winner. The current game board is displayed after each move and then asking for the input
+  from another player. The program structure separates functionality into modular functions, promoting readability and maintainability.
+ */
+
+// The size of the board
 #define NUM_BOARD_SPACES 25
 
+// Check the valid of movements
 int isValidMove(const char *initial_board, int row, int col) {
     if (row < 0 || row > 4 || col < 0 || col > 4) {
-        printf("Invalid move data!\n");
         return 0;
     }
 
@@ -18,6 +31,7 @@ int isValidMove(const char *initial_board, int row, int col) {
     return 1;
 }
 
+// Update the surrounding cells
 void updateSurroundingCells(char *initial_board, int row, int col) {
     int directions[8][2] = {
             {-1, 0}, {-1, -1}, {-1, 1},
@@ -38,6 +52,7 @@ void updateSurroundingCells(char *initial_board, int row, int col) {
     }
 }
 
+// Show the game-board
 void displayBoard(const char *initial_board) {
     printf("  01234\n");
     printf(" *******\n");
@@ -65,6 +80,7 @@ void displayBoard(const char *initial_board) {
     printf(" *******\n");
 }
 
+// Check whether the game over or not
 int isGameOver(const char *initial_board) {
     for (int i = 0; i < 25; i++) {
         if (initial_board[i] == 'E') {
@@ -78,39 +94,54 @@ int main(int argc, char *argv[]) {
     printf("Welcome to Bricker!\n");
 
     char initial_board[NUM_BOARD_SPACES + 1];
+    // Initialize the board
     memset(initial_board, 'E', 25);
     initial_board[25] = '\0';
 
+    // If the number of arguments is two and the length of second argument is twenty-five, customize the initial board
+    // using parameters from command lines
     if (argc == 2 && strlen(argv[1]) == 25) {
         strncpy(initial_board, argv[1], 25);
     }
 
+    // Show the initial board
     displayBoard(initial_board);
 
+    // Start from player 1
     int currentPlayer = 1;
     int row, col;
+    // Store the data of the last player
     int lastPlayer = 0;
 
+    // Loop for the game (take turns) (with condition)
     while (!isGameOver(initial_board)) {
 
+        // Player take turns enter the data of row and column
         printf("Player %d turn: ", currentPlayer);
         scanf("%d %d", &row, &col);
 
+        // Logic operation with condition to judge the valid of entered row and column
         if (isValidMove(initial_board, row, col)) {
+            // Put the corresponding sign of player on give position
             char playerToken = (currentPlayer == 1) ? 'X' : 'O';
             int index = row * 5 + col;
             initial_board[index] = playerToken;
+            // Update surrounding cells
             updateSurroundingCells(initial_board, row, col);
             lastPlayer = currentPlayer;
+            // Change player
             currentPlayer = (currentPlayer == 1) ? 2 : 1;
 
+            // Show game-board when condition is TRUE
             displayBoard(initial_board);
         } else {
+            // When the condition is not TRUE, print message and skip printing the current game-board
             printf("Invalid Move!\n");
             continue;
         }
     }
-    
+
+    // Judge the winner when the game loop over
     if (lastPlayer != 0) {
         printf("Player %d Wins!\n", lastPlayer);
     }
